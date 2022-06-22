185c7ebb6419ef04c336da13c2a7a0c670fd7f7f
Yuri Onório <101823180+YuriOnorio@users.noreply.github.com>
2022-06-22 14:53:36 -0300

    adicionamento do tab4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,15 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'tablinks',
+    loadChildren: () => import('./tablinks/tablinks.module').then( m => m.TablinksPageModule)
   }
+
 ];
 @NgModule({
   imports: [
